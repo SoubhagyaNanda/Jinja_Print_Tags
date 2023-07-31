ec40f0ace0398a087617232a5b82852e11dba180
SoubhagyaNanda <soubhagyaranjannanda360@gmail.com>
2023-07-31 20:22:05 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -7,5 +7,5 @@ def data_render(request):
     return render(request,'data_render.html',context=d)
 
 def cond(request):
-    d={'A':10,'B':50, 'C':30}
+    d={'name':'Jai ShreeRam', 'A':10,'B':50, 'C':30}
     return render(request,'cond.html',context=d)
