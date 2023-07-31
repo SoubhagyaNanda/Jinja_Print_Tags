1e32fa25437a871fc906350af56dee73e2752249
SoubhagyaNanda <soubhagyaranjannanda360@gmail.com>
2023-07-31 09:47:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def data_render(request):
     d={'name':'RAM','age':3}
     return render(request,'data_render.html',context=d)
+
+def cond(request):
+    d={'A':10,'B':50, 'C':30}
+    return render(request,'cond.html',context=d)
